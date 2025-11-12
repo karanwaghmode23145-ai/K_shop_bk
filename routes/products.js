b8108d873df8cf5c54e6769c230876c4669ccd67
waghmodekaran5595 <waghmodekaran5595@gmail.com>
2025-11-12 17:59:06 +0530

    fix: move api folder to root

--- routes/products.js
+++ routes/products.js
@@ -10,22 +10,4 @@ router.get("/", (req, res) => {
   res.json(products);
 });
 
-router.get("/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const product = products.find((p) => p.id === id);
-  if (!product) return res.status(404).json({ error: "Product not found" });
-  res.json(product);
-});
-
-router.post("/", (req, res) => {
-  const { name, price } = req.body;
-  if (!name || typeof price !== "number")
-    return res.status(400).json({ error: "Invalid data" });
-
-  const newProduct = { id: Date.now(), name, price };
-  products.push(newProduct);
-  res.status(201).json(newProduct);
-});
-
 module.exports = router;
-
